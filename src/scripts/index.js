8cd1371d970aa35a2d07b175fe212da707c4db65
evdrug <johnydj@yandex.ru>
2017-09-10 17:39:13 +0300

    +btnIndex

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,12 +1,20 @@
 var btnAvt = document.getElementsByClassName('btn-singin'),
-    blockAvt = document.getElementsByClassName('wrapper__form');
+    blockAvt = document.getElementsByClassName('wrapper__form'),
+    btnIndex = document.getElementsByClassName('index');
 
     btnAvt = btnAvt[0];
     blockAvt = blockAvt[0];
+    btnIndex = btnIndex[0];
 
 
 btnAvt.onclick = function (e) {
     e.preventDefault();
     blockAvt.classList.add('flipped')
     btnAvt.classList.add('none');
+}
+
+btnIndex.onclick = function (e) {
+    e.preventDefault();
+    blockAvt.classList.remove('flipped')
+    btnAvt.classList.remove('none');
 }
