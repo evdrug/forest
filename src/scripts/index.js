0e9fd4e272d6aaebd4a2ac819985b6c4c46433e0
evdrug <johnydj@yandex.ru>
2017-09-17 21:09:49 +0300

    adaptive

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,23 +3,50 @@
 
 var btnAvt = document.getElementsByClassName('btn-avt'),
     blockAvt = document.getElementsByClassName('welcome__form-wrapper'),
-    btnIndex = document.getElementsByClassName('index');
+    btnIndex = document.getElementsByClassName('index'),
+    btnHumb = document.getElementsByClassName('hamburger__link'),
+    menHumb = document.getElementsByClassName('meniHamb'),
+    iconHumb = document.getElementsByClassName('hamburger');
 
     btnAvt = btnAvt[0];
     blockAvt = blockAvt[0];
     btnIndex = btnIndex[0];
+    btnHumb =   btnHumb[0];
+    menHumb = menHumb[0];
+    iconHumb = iconHumb[0];
 
+console.log(btnHumb);
 
-btnAvt.onclick = function (e) {
-    e.preventDefault();
-    blockAvt.classList.add('flipped')
-    btnAvt.classList.add('none');
+if(btnAvt){
+    btnAvt.onclick = function (e) {
+        e.preventDefault();
+        blockAvt.classList.add('flipped')
+        btnAvt.classList.add('none');
+    }
 }
 
-btnIndex.onclick = function (e) {
+if(btnIndex){
+    btnIndex.onclick = function (e) {
+        e.preventDefault();
+        blockAvt.classList.remove('flipped')
+        btnAvt.classList.remove('none');
+    }
+}
+
+
+btnHumb.onclick = function (e) {
     e.preventDefault();
-    blockAvt.classList.remove('flipped')
-    btnAvt.classList.remove('none');
+    iconHumbAct = document.getElementsByClassName('hamburger_active');
+    if(iconHumbAct[0]) {
+        menHumb.classList.remove('meniHamb_active')
+        iconHumb.classList.remove('hamburger_active');
+        document.body.style.overflow = 'auto';
+    }else{
+        menHumb.classList.add('meniHamb_active')
+        iconHumb.classList.add('hamburger_active');
+        document.body.style.overflow = 'hidden';
+    }
+
 }
 
 
