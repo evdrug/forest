ec4f27d73152bca93f84efb523a75c9e02a74730
evdrug <e.druzyakin@gmail.com>
2017-09-18 10:32:28 +0300

    other2

--- dst/js/bundle.js
+++ dst/js/bundle.js
@@ -75,28 +75,53 @@ module.exports = __webpack_require__(2);
 /* 1 */
 /***/ (function(module, exports) {
 
-// import './map'
 
 
 var btnAvt = document.getElementsByClassName('btn-avt'),
     blockAvt = document.getElementsByClassName('welcome__form-wrapper'),
-    btnIndex = document.getElementsByClassName('index');
+    btnIndex = document.getElementsByClassName('index'),
+    btnHumb = document.getElementsByClassName('hamburger__link'),
+    menHumb = document.getElementsByClassName('meniHamb'),
+    iconHumb = document.getElementsByClassName('hamburger');
 
     btnAvt = btnAvt[0];
     blockAvt = blockAvt[0];
     btnIndex = btnIndex[0];
+    btnHumb =   btnHumb[0];
+    menHumb = menHumb[0];
+    iconHumb = iconHumb[0];
 
 
-btnAvt.onclick = function (e) {
-    e.preventDefault();
-    blockAvt.classList.add('flipped')
-    btnAvt.classList.add('none');
+if(btnAvt){
+    btnAvt.onclick = function (e) {
+        e.preventDefault();
+        blockAvt.classList.add('flipped')
+        btnAvt.classList.add('none');
+    }
 }
 
-btnIndex.onclick = function (e) {
+if(btnIndex){
+    btnIndex.onclick = function (e) {
+        e.preventDefault();
+        blockAvt.classList.remove('flipped')
+        btnAvt.classList.remove('none');
+    }
+}
+
+
+btnHumb.onclick = function (e) {
     e.preventDefault();
-    blockAvt.classList.remove('flipped')
-    btnAvt.classList.remove('none');
+    iconHumbAct = document.getElementsByClassName('hamburger_active');
+    if(iconHumbAct[0]) {
+        menHumb.classList.remove('meniHamb_active')
+        iconHumb.classList.remove('hamburger_active');
+        document.body.style.overflow = 'auto';
+    }else{
+        menHumb.classList.add('meniHamb_active')
+        iconHumb.classList.add('hamburger_active');
+        document.body.style.overflow = 'hidden';
+    }
+
 }
 
 
@@ -106,40 +131,40 @@ btnIndex.onclick = function (e) {
 /* 2 */
 /***/ (function(module, exports) {
 
-// var map;
-//
-//
-//
-//
-//
-//
-// function initMap() {
-//
-//     var pos = {lat: 59.937763, lng: 30.218649};
-//     var pos1 = {lat: 59.937220, lng: 30.224896};
-//     var marker_url = "/images/map-marker.png";
-//     var popupContent = '<p class="content">Что угодно</p>';
-//
-//     var styles = [{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#87a87b"},{"visibility":"on"}]}]
-//
-//
-//     map = new google.maps.Map(document.getElementById('map'), {
-//         center: pos,
-//         zoom: 16,
-//         disableDefaultUI: true,
-//         scrollwheel: false
-//     });
-//     var marker = new google.maps.Marker({
-//         position: pos1,
-//         map: map,
-//         icon: marker_url,
-//         animation: google.maps.Animation.BOUNCE
-//     });
-//
-//     map.setOptions({styles: styles});
-// }
-//
-// initMap();
+var map;
+
+
+
+
+
+
+function initMap() {
+
+    var pos = {lat: 59.937763, lng: 30.218649};
+    var pos1 = {lat: 59.937220, lng: 30.224896};
+    var marker_url = "/images/map-marker.png";
+    var popupContent = '<p class="content">Что угодно</p>';
+
+    var styles = [{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#87a87b"},{"visibility":"on"}]}]
+
+
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: pos,
+        zoom: 16,
+        disableDefaultUI: true,
+        scrollwheel: false
+    });
+    var marker = new google.maps.Marker({
+        position: pos1,
+        map: map,
+        icon: marker_url,
+        animation: google.maps.Animation.BOUNCE
+    });
+
+    map.setOptions({styles: styles});
+}
+
+initMap();
 
 /***/ })
 /******/ ]);
