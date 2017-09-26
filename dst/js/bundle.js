71b2ebc34db7d13afd17e6574f1dda6b7dc3c991
evdrug <e.druzyakin@gmail.com>
2017-09-26 23:00:22 +0300

    scroll

--- dst/js/bundle.js
+++ dst/js/bundle.js
@@ -85,13 +85,19 @@ var btnAvt = document.getElementsByClassName('btn-avt'),
     btnIndex = document.getElementsByClassName('index'),
     menHumb = document.getElementsByClassName('menu-hamb'),
     iconHumb = document.getElementsByClassName('hamburger'),
-    preloader = document.getElementById('preloader-page');
+    preloader = document.getElementById('preloader-page'),
+    bg = document.getElementsByClassName('flipp'),
+    formInput = document.getElementsByClassName('fotm__input-text'),
+    btnArrow = document.getElementsByClassName('btn-arrow'),
+    arrowUp = document.getElementsByClassName('arrow_up'),
+    // titleH1 = document.getElementsByTagName('h1');
+    titleH1 = document.getElementsByClassName('block-triangle'),
+    worksWrapper = document.getElementsByClassName('works-wrapper');
 
 
 
 window.onload = ()=> {
     preloader.setAttribute('style', 'display: none;');
-
 
 
     var btnHumb = document.getElementsByClassName('hamburger__link');
@@ -102,11 +108,22 @@ window.onload = ()=> {
     btnHumb =   btnHumb[0];
     menHumb = menHumb[0];
     iconHumb = iconHumb[0];
+    bg = bg[0];
+    formInput = formInput[0];
+    btnArrow = btnArrow[0];
+    arrowUp = arrowUp[0];
+    titleH1 = titleH1[0];
+    worksWrapper = worksWrapper[0];
+    var pos = titleH1.getBoundingClientRect();
 
 
 
-
-
+    if(bg){
+        bg.onclick = function (e) {
+            blockAvt.classList.remove('flipped')
+            btnAvt.classList.remove('none');
+        }
+    }
     if(btnAvt){
         btnAvt.onclick = function (e) {
             e.preventDefault();
@@ -122,6 +139,7 @@ window.onload = ()=> {
             btnAvt.classList.remove('none');
         }
     }
+
 
 
     if(btnHumb) {
@@ -145,6 +163,36 @@ window.onload = ()=> {
                 }
             }
 
+        }
+    }
+
+    if(btnArrow){
+        btnArrow.onclick = function (e) {
+            e.preventDefault();
+            windPos = window.pageYOffset;
+            var coord = pos.top+pos.height-20;
+            var top = windPos;
+            var scr = setInterval(function () {
+                top += 5;
+                window.scrollTo(0, top);
+                if (top > coord) {
+                    clearInterval(scr);
+                }
+            }, 5);
+        }
+    }
+    if(arrowUp){
+        arrowUp.onclick = function (e) {
+            e.preventDefault();
+            windPos = window.pageYOffset;
+            var coord = pos.top+pos.height-20;
+            var scr = setInterval(function () {
+                windPos -= 5;
+                window.scrollTo(0, windPos);
+                if (windPos < coord) {
+                    clearInterval(scr);
+                }
+            }, 2);
         }
     }
 }
